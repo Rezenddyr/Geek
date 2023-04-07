c97d7dc5b988231f3811698f1e2c6cff8c5304b2
Rezendyr <107434315+Rezenddyr@users.noreply.github.com>
2023-04-07 12:44:45 -0300

    css e js

---
+++ js/main.js
@@ -0,0 +1,63 @@
+(function ($) {
+    "use strict";
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Fixed Navbar
+    $(window).scroll(function () {
+        if ($(window).width() < 992) {
+            if ($(this).scrollTop() > 45) {
+                $('.fixed-top').addClass('bg-dark shadow');
+            } else {
+                $('.fixed-top').removeClass('bg-dark shadow');
+            }
+        } else {
+            if ($(this).scrollTop() > 45) {
+                $('.fixed-top').addClass('bg-dark shadow').css('top', 0);
+            } else {
+                $('.fixed-top').removeClass('bg-dark shadow').css('top', 0);
+            }
+        }
+    });
+
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: [
+          '<i class="bi bi-arrow-left"></i>',
+          '<i class="bi bi-arrow-right"></i>',
+        ],
+      });
+    
+      $(".vendor-carousel").owlCarousel({
+        loop: true,
+        margin: 45,
+        dots: false,
+        loop: true,
+        autoplay: true,
+        smartSpeed: 1000,
+        responsive: {
+          0: {
+            items: 2,
+          },
+          576: {
+            items: 4,
+          },
+          768: {
+            items: 6,
+          },
+          992: {
+            items: 8,
+          },
+        },
+      });
+    
+})(jQuery);
+
